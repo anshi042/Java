8c139b43c125f9dda2c9c99afbeb280bcd448613
Anshi Sharma <125981566+anshi042@users.noreply.github.com>
2024-02-13 15:08:31 +0530

    Update printXn.java

--- printXn.java
+++ printXn.java
@@ -1,5 +1,5 @@
-
 public class printXn {
+    //find X^n using recursion with time complexity O(n)
     public static int power(int x, int n){
         if(n == 1){
             return x;
@@ -10,9 +10,25 @@ public class printXn {
 
         return x * power(x, n-1);
     }
+
+    //Optimized solution with time complexity O(log n)
+    public static int pow(int x, int n){
+        if(n ==0){
+            return 1;
+        }
+        int halfPowSq = pow(x, n/2) * pow(x, n/2);
+
+        // when n is odd
+        if(n % 2 != 0){
+            halfPowSq = x * halfPowSq;
+        }
+        return halfPowSq;
+    }
+    
     public static void main(String[] args){
         int x = 2;
         int n = 10;
-        System.out.println(power(x, n));
+        //System.out.println(power(x, n));
+        System.out.println(pow(x, n));
     }
 }
